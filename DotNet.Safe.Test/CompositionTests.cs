f9bd2d68209959428e44fcf7910343ef94224bc5
Carlos Lubián <carlubian@outlook.es>
2018-07-01 20:05:17 +0200

    Minor additional test

--- DotNet.Safe.Test/CompositionTests.cs
+++ DotNet.Safe.Test/CompositionTests.cs
@@ -49,10 +49,26 @@ namespace DotNet.Safe.Test
                 .And.Match<Either<int>>(n => n.ErrorOrElse("") == "Error");
         }
 
+        [TestMethod]
+        public void TestEitherUnwrap()
+        {
+            Try.This(GetEither).Now()
+                .Should().BeOfType(typeof(Success<int>))
+                .And.NotBeOfType(typeof(Success<Success<int>>));
+
+            Try.This(GetFailure).Now()
+                .Should().BeOfType(typeof(Failure<int>))
+                .And.NotBeOfType(typeof(Success<Failure<int>>));
+        }
+
         private int GetNumber() => 2;
 
         private int GetFaulty() => throw new System.Exception("Error");
 
         private int Multiply(int n) => n * 2;
+
+        private Either<int> GetEither() => new Success<int>(1024);
+
+        private Either<int> GetFailure() => new Failure<int>("Kaboom!");
     }
 }
