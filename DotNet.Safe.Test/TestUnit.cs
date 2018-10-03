290a3f08088ec9fb3dd1a6a5dffd6ca37b700c4c
Carlos Lubián <carlubian@outlook.es>
2018-10-03 11:02:12 +0200

    Updated test project dependencies; Additional tests.

---
+++ DotNet.Safe.Test/TestUnit.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using DotNet.Safe.Standard.Util;
+
+namespace DotNet.Safe.Test
+{
+    [TestClass]
+    public class TestUnit
+    {
+        [TestMethod]
+        public void TestUnitHashcode()
+        {
+            Unit.Instance().GetHashCode()
+                .Should().Be(0);
+        }
+
+        [TestMethod]
+        public void TestUnitToString()
+        {
+            Unit.Instance().ToString()
+                .Should().Be("[Unit]");
+        }
+
+        [TestMethod]
+        public void TestUnitEquals()
+        {
+            Unit.Instance().Equals(Unit.Instance())
+                .Should().BeTrue();
+
+            Unit.Instance().Equals(Unit.Instance() as object)
+                .Should().BeTrue();
+
+            Unit.Instance().Equals(new object())
+                .Should().BeFalse();
+
+            Unit.Instance().Equals(0)
+                .Should().BeFalse();
+        }
+    }
+}
